ef03d572ac47473ee914dd0313abd0f33ab680a3
Madhuri Pokala <madhurid0309@gmail.com>
2026-08-08 23:38:23 +0100

    GET connections API with validations

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,16 +13,19 @@ userRouter.get(
       const pendingConnectionRequests = await ConnectionRequests.find({
         toUserId: loggedinUser._id,
         status: "interested",
-      }).populate("fromUserId", [
-        "firstName",
-        "lastName",
-        "age",
-        "gender",
-        "skills",
-        "localtion",
-        "photoUrl",
-        "about",
-      ]);
+      }).populate(
+        ["fromUserId", "toUserId"],
+        [
+          "firstName",
+          "lastName",
+          "age",
+          "gender",
+          "skills",
+          "localtion",
+          "photoUrl",
+          "about",
+        ],
+      );
       if (pendingConnectionRequests.length == 0) {
         return res.json({ message: "There are no pending requests." });
       }
@@ -35,5 +38,41 @@ userRouter.get(
     }
   },
 );
+
+// connections established
+userRouter.get("/user/connections", userAuthMiddleware, async (req, res) => {
+  try {
+    const loggedinUser = req.user;
+
+    // ConnectionRequests.find($or: [{ status: "accepted",fromUserId: loggedinUser._id }, { status: "accepted",toUserId: loggedinUser._id }])
+
+    const activeConnections = await ConnectionRequests.find({
+      $or: [{ fromUserId: loggedinUser._id }, { toUserId: loggedinUser._id }],
+      status: "accepted",
+    }).populate(
+      ["fromUserId", "toUserId"],
+      "firstName lastName age skills about gender location photoUrl",
+    );
+    let data = activeConnections;
+    if (activeConnections.length == 0) {
+      return res.json({
+        message: `${loggedinUser.firstName} do not have any active connections yet!`,
+      });
+    }
+    data = activeConnections.map((obj) => {
+      if (obj.fromUserId._id.equals(loggedinUser._id)) {
+        return obj.toUserId;
+      }
+      return obj.fromUserId;
+    });
+
+    return res.json({
+      message: "Connections fetched successfully",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).send("Error: " + error.message);
+  }
+});
 
 module.exports = userRouter;
